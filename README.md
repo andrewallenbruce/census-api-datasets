e6f51531762bf2870b9a11e4360339781221a70b
Hannah Recht <hrecht@users.noreply.github.com>
2025-08-11 10:13:00 -0400

    social meta

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Census API dataset tracker
 
-This project tracks the dataset endpoints available in the U.S. Census Bureau APIs. There are no publicly available changelogs or regular announcements of added or removed endpoints. This daily scraper aims to fill that gap.
+This project tracks the dataset endpoints available in the U.S. Census Bureau APIs. There are no publicly available changelogs or regular announcements of added or removed endpoints. This scraper aims to fill that gap.
 
+[See the tracker](https://www.hrecht.com/census-api-datasets/)
+
+## How it works
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script>
 	//import favicon from '$lib/assets/favicon.svg';
-	import social from '$lib/assets/social-image-og.jpg';
+	import social from '$lib/assets/social-image-og.png';
 	import '../styles/style.css';
 
-	let social_description = "My page social meta description."
+	let social_description = "See when the U.S. Census Bureau adds or removes datasets."
 	let page_title = "Tracking Census Dataset Changes"
 
 	let { children } = $props();
@@ -12,23 +12,23 @@
 <svelte:head>
 	<!--link rel="icon" href={favicon} /-->
 
-	<link rel="canonical" href="https://www.hrecht.com/">
+	<link rel="canonical" href="https://www.hrecht.com/census-api-datasets/">
 
 	<meta name="description" content={social_description}>
 	<meta name="author" content="Hannah Recht">
 	<meta name="news_keywords" content="census, data, federal government, census api, api">
 	<meta property="og:title" content={page_title}>
 	<meta property="og:site_name" content="Hannah Recht">
-	<meta property="og:url" content="https://www.hrecht.com/">
+	<meta property="og:url" content="https://www.hrecht.com/census-api-datasets/">
 	<meta property="og:description" content={social_description}>
 	<meta property="og:type" content="article">
 	<meta property="og:locale" content="en_US">
 	<meta property="og:image" content={social}>
 	<meta property="og:image:type" content="image/png">
 	<meta property="og:image:width" content="1200">
-	<meta property="og:image:height" content="628">
+	<meta property="og:image:height" content="630">
 	<meta name="twitter:card" content="summary_large_image">
-	<meta name="twitter:site" content="https://www.hrecht.com/">
+	<meta name="twitter:site" content="https://www.hrecht.com/census-api-datasets/">
 	<meta name="twitter:creator" content="@hannah_recht">
 	<meta name="twitter:title" content={page_title}>
 	<meta name="twitter:description" content={social_description}>

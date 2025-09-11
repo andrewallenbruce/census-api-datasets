63bac306abf71a98da38f2c961b0fba66af80405
Hannah Recht <hrecht@users.noreply.github.com>
2025-09-11 09:20:34 -0400

    turn off test mode

--- scripts/update-settings.R
+++ scripts/update-settings.R
@@ -1,12 +1,12 @@
 # Settings for the metadata comparison run
 
 # Should changes be committed to Github?
-commit_changes <- F
+commit_changes <- T
 
 # Test FAKE dataset changes
 # commit_changes should be set to FALSE if this is TRUE
 # to avoid building the site and posting with FAKE data
-data_test <- T
+data_test <- F
 
 # Revert a prior test run (for logging purposes)
 test_revert <- F

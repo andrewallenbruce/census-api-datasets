1f96c12ba36ab217c8ffc233aaf5803cb55bf8d8
Hannah Recht <hrecht@users.noreply.github.com>
2025-09-11 08:23:13 -0400

    test post

--- scripts/update-settings.R
+++ scripts/update-settings.R
@@ -1,12 +1,12 @@
 # Settings for the metadata comparison run
 
 # Should changes be committed to Github?
-commit_changes <- T
+commit_changes <- F
 
 # Test FAKE dataset changes
 # commit_changes should be set to FALSE if this is TRUE
 # to avoid building the site and posting with FAKE data
-data_test <- F
+data_test <- T
 
 # Revert a prior test run (for logging purposes)
 test_revert <- F
